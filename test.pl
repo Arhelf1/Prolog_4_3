3f3a6fd2d01db5f807956e957082010d049a675e
Arhelf1 <arhelf1@gmail.com>
2020-12-02 20:02:27 +0300

    v. 0.3

--- test.pl
+++ test.pl
@@ -1,45 +1,48 @@
 %Задача про трех студентов рому петю и серегу, которые учатся на трех разных факультетах
 %Нужно узнать кто рома и даны условия
-
 fac(physics).
-fac(mathematics).
 fac(chemistry).
+fac(mathematics).
 %Продолжаем писать код
 
 %Петя математик -> Серега не физик 
-
-uslovie1(Stud1, Stud2):- 
-	Stud1 = mathematics,
-	not(Stud2 = physics).
+uslovie1(Stud1, Stud2) :-
+  Stud1 = mathematics,
+  not(Stud2 = physics).
+  
 %иначе
-uslovie1(Stud1,_):-
-	not(Stud1 = mathematics).
-
+uslovie1(Stud1, _) :-
+  not(Stud1 = mathematics).
 %Рома не физик -> Петя математик
+uslovie2(Stud1, Stud2) :-
+  not(Stud1 = physics),
+  Stud2 = mathematics.
 
-uslovie2(Stud1, Stud2):- 
-	not(Stud1 = physics),
-	Stud2 = mathematics.
-uslovie2(phisics,_).
-%Не знаю почему оно работало но в строке выше была опечатка :D 
-
+uslovie2(physics, _).
 % серега не математик -> рома химик
-
-uslovie3(Stud1, Stud2):-
-	not(Stud1 = mathematics)'
-	Stud2 = chemistry.
-uslovie3(mathematics,_).
+uslovie3(Stud1, Stud2) :-
+  not(Stud1 = mathematics),
+  Stud2 = chemistry.
+  
+uslovie3(mathematics, _).
 
 %Процедура для решения 
-proc (Stud1, Stud2, Stud3):- 
-	fac(Stud1),
-	fac(Stud2),
-	fac(Stud3),
-uslovie1(Stud1, Stud3),
-uslovie2(Stud2, Stud1),
-uslovie3(Stud3, Stud2).
+solve(Stud1, Stud2, Stud3) :-  
+  fac(Stud1),
+  fac(Stud2),
+  fac(Stud3), 
+  uslovie1(Stud1, Stud3),
+  uslovie2(Stud2, Stud1),
+  uslovie3(Stud3, Stud2).
 
-main:-
-	proc(Petr,Roma,Serega),
-	write("Рома - "),
-	write(Roma), !.
+main :-
+  solve(Petr, Roma, Serega),
+  write("Roman is "),
+  write(Roma), 
+  !.
+%-------------------------
+
+
+  
+  
+  
